d6846061ef111876cd13cded662041c8bfa49333
Nikolay Dodatko <n.y.dodatko@gmail.com>
2021-07-05 16:37:10 +0300

    Убран файл базы, отработаны конфликты в root_routes.py

--- project/root_routes.py
+++ project/root_routes.py
@@ -1,6 +1,4 @@
 from flask import Blueprint
-<<<<<<< HEAD
-=======
 from project.recipes.routes import recipes_api_bp  # products_bp
 from project.parsers.routes import goods_api_bp  # goods_bp
 
@@ -10,14 +8,11 @@ api.register_blueprint(recipes_api_bp)
 
 content = Blueprint('content', __name__)
 # content.register_blueprint(products_bp)
->>>>>>> ac3bcc4a01ac9ce2da417465652ead804e3a8c72
 
 from project.recipes.routes import recipes_bp
 
 root = Blueprint('root', __name__,)
-<<<<<<< HEAD
 root.register_blueprint(recipes_bp)
-=======
 root.register_blueprint(api)
 root.register_blueprint(content)
 
@@ -26,4 +21,3 @@ root.register_blueprint(content)
 # root в приложении app.
 # Можно было отдельно зарег-ть базу в приложении
 root.register_blueprint(goods_api_bp)
->>>>>>> ac3bcc4a01ac9ce2da417465652ead804e3a8c72
